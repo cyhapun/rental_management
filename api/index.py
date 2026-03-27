b478195339c36d5b52affb7c8040a094c6881c8f
cyhapun <cyhapun242@gmail.com>
2026-03-28 02:05:55 +0700

    fix: fix to deploy on vercel

--- api/index.py
+++ api/index.py
@@ -1,4 +1,11 @@
+import os
+import sys
+
+proj_root = os.path.dirname(os.path.dirname(__file__))
+if proj_root not in sys.path:
+	sys.path.insert(0, proj_root)
+
 from mangum import Mangum
-from main import app
+from app.main import app
 
 handler = Mangum(app)
